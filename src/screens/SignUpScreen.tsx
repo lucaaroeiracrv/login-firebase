654f653ec4beb7c47a1aca7ea547f023d50ef0ea
lucaaroeiracrv <lucaaroeira@gmail.com>
2024-10-03 00:23:46 -0300

    fix: define type of navigation and optimize layout

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -16,7 +16,7 @@ const SignUpScreen: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
-  const navigation = useNavigation();
+  const navigation = useNavigation<SignUpScreenNavigationProp>();
 
   const handleCreateAccount = () => {
     if (password !== repeatPassword) {
@@ -55,10 +55,26 @@ const SignUpScreen: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', padding: 20 },
-  title: { fontSize: 24, textAlign: 'center', marginBottom: 20 },
-  backButton: { marginTop: 20, alignItems: 'center' },
-  backButtonText: { color: '#6200EE', fontSize: 16 }
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    padding: 20, 
+    backgroundColor: '#d3d0cc',
+},
+title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#3D3D3DFF',
+  },
+  backButton: { 
+    marginTop: 20,
+     alignItems: 'center' 
+    },
+  backButtonText: { 
+    color: '#6200EE',
+    fontWeight: 'bold',
+},
 });
 
 export default SignUpScreen;
